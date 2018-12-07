68eb87314ba102d23587006ffe72fd282dfb0eb8
tsabunkar <tsabunkar@gmail.com>
2018-12-07 18:16:46 +0530

    first commit

---
+++ src/app/users-list/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  url = 'https://api.github.com/users';
+
+  constructor(private _http: HttpClient) { }
+
+  getAllUsers() {
+    return this._http.get(this.url);
+  }
+
+  getUser(searchedQuery) {
+    return this._http.get(`https://api.github.com/search/users?q=${searchedQuery}`);
+    /*   .pipe(
+        tap(data => {
+          console.log(data);
+        })
+      ); */
+  }
+
+}
+
+
+
+
